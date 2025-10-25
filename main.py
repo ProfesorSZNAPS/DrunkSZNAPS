b0b738d68bea874f9014f8885c586645064a62ac
ProfesorSZNAPS <okbartek44@gmail.com>
2025-10-25 10:59:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,59 @@
-import discord
-import random
-from discord.ext import commands
+# Import
+from flask import Flask, render_template, request, send_from_directory
 
-intents = discord.Intents.default()
-intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+app = Flask(__name__)
 
-@bot.event
-async def on_ready():
-    print(f'Zalogowales sie jako {bot.user}')
 
-@bot.command()
-async def mem(ctx):
-    with open('images\mem1.jpg', 'rb') as f:
-        picture = discord.File(f)
-    with open('images\mem2.jpg', 'rb') as h:
-        picture = discord.File(h)
-    with open('images\mem3.jpg', 'rb') as g:
-        picture = discord.File(g)
-    o=random.randint(1,3)
-    if o==1:
-        picture = discord.File(f)
-    elif o==2:
-        picture = discord.File(h)
-    elif o==3:
-        picture = discord.File(g)
-    await ctx.send(file=picture)
 
-bot.run("TOKEN")
+
+# Formularz z rezultatami
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        # odczytywanie wybranego obrazka
+        selected_image = request.form.get('image-selector')
+        text_top= request.form.get('text_top')
+        text_bottom = request.form.get('text_bottom')
+        text_bottom_y = request.form.get('text_bottom_y')
+        text_top_y = request.form.get('text_top_y')
+        selected_color = request.form.get('selected_color')
+        text_top_y = text_top_y
+        text_bottom_y = text_bottom_y
+        selected_color = selected_color
+
+
+        # Zadanie #2. Odczytywanie tekstu
+        
+        # Zadanie #3. Odczytywanie pozycji tekstu
+       
+
+        # Zadanie #3. Odczytywanie koloru tekstu
+        
+
+        return render_template('index.html', 
+                               # Wyświetlanie wybranego obrazka
+                               selected_image=selected_image, 
+
+                               # Zadanie #2. Wyświetlanie tekstu
+                               text_top=text_top,
+                               text_bottom=text_bottom,
+
+                               # Zadanie #3. Wyświetlanie koloru
+                               selected_color=selected_color,
+                               
+                               # Zadanie #3. Wyświetlanie pozycji tekstu
+                               text_bottom_y=text_bottom_y,
+                               text_top_y=text_top_y
+
+                               )
+    else:
+        # Wyświetlanie pierwszego obrazka, jako grafika domyślna
+        return render_template('index.html', selected_image='logo.svg')
+
+
+@app.route('/static/img/<path:path>')
+def serve_images(path):
+    return send_from_directory('static/img', path)
+
+app.run(debug=True)
